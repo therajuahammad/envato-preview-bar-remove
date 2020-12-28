5204370916f69137c43f986318b82ff1d5d4ab95
Raju Ahammad <rajuahammad73@gmail.com>
2020-12-28 16:53:30 +0600

    close button update

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const URL = document.querySelector('.full-screen-preview__frame');
 const button = document.createElement('a');
-button.innerText = 'Close';
-button.className = 'header-buy-now e-btn--3d -color-default';
+button.innerText = 'X';
+button.className = 'header-close-now e-btn--3d -color-default';
 button.href = URL.attributes.src.value;
 
 document.querySelector('.preview__action--buy').appendChild(button);
